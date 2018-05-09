db502102a739f13c46b29c69702165564eeb755c
tmukhadgverdeli <37073128+tmukhadgverdeli@users.noreply.github.com>
2018-05-09 20:53:12 +0400

    6

---
+++ js/script.js
@@ -0,0 +1,39 @@
+
+$(document).ready(function(){
+// alert('123');
+
+
+ $("#burger").click(function(){
+        $("#burger").hide();
+        $(".nav_mobile").fadeIn();
+
+    });
+ $(".mob_button").click(function(){
+        $("#burger").show();
+        $(".nav_mobile").fadeOut();
+
+    });
+ $(".moobile_a").click(function(){
+        $("#burger").show();
+        $(".nav_mobile").fadeOut();
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
